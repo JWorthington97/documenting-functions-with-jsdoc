219897db2a1dddf6802d3025f37e3abbbf80579f
JWorthington97 <jb.worthington@live.co.uk>
2021-06-02 16:48:38 +0100

    Document asNegative

--- challenges/0-document.js
+++ challenges/0-document.js
@@ -1,7 +1,20 @@
 // TODO (Ex.0): document and write tests for this function
 
+/**
+ * Convert an integer to the negative equivalent (if not already negative)
+ * @param {integer} n - input number
+ * @returns {integer} negative form of `n`, or `n` if already negative
+ */
 function asNegative(n) {
-  // ...
+  if (n > 0) { //if (n >= 0) { - illustrates -0
+    return n*-1
+  }
+  return n
 }
 
-
+console.log(-1, asNegative(1))
+console.log(-1, asNegative(-1))
+console.log(0, asNegative(0))
+console.log(-1.5, asNegative(1.5))
+console.log(-1, asNegative('1'))
+console.log(undefined, asNegative())
